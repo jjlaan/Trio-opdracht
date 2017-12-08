735c8d34167810ec704c8a256458875edab3c6d2
jjlaan <jj.vanderlaan@student.avans>
2017-12-08 11:48:07 +0100

    Basissetup Trio-opdracht

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
 public static void main(String[] args) {
-    System.out.println("This sucks");
+    System.out.println("This sucks a bit less now that it actually seems to work");
     }
 }
